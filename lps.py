820cf3526f9662e161cf0750dcfa82660eee4da4
Abhishek Verma <49243173+AbhishekVerma023@users.noreply.github.com>
2019-09-21 00:26:32 +0530

    Create lps.py

---
+++ lps.py
@@ -0,0 +1,15 @@
+def computeLPSArray(pat, M, lps):
+    len = 0
+    lps[0]
+    i = 1
+    while i < M:
+        if pat[i] == pat[len]:
+            len += 1
+            lps[i] = len
+            i += 1
+        else:
+            if len != 0:
+                len = lps[len-1]
+            else:
+                lps[i] = 0
+                i += 1
